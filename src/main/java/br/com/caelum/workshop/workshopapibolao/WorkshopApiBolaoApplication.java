4faa3cc272298cdded783ec7a5f2f6fcbd313f12
asouza <alots.ssa@gmail.com>
2019-12-13 19:46:42 -0300

    setup inicial

---
+++ src/main/java/br/com/caelum/workshop/workshopapibolao/WorkshopApiBolaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.caelum.workshop.workshopapibolao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopApiBolaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopApiBolaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/caelum/workshop/workshopapibolao/WorkshopApiBolaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.caelum.workshop.workshopapibolao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopApiBolaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
